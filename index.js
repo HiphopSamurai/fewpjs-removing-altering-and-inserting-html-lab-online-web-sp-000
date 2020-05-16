48009cdf150f2f70837df7ea50dceb019c939020
Garrett Thompson <gtcruz.gt@gmail.com>
2020-05-16 00:00:47 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -2,3 +2,7 @@
 
 let elementMain = document.querySelector("main#main");
 elementMain.remove();
+
+var newHeader = document.createElement("h1");
+newHeader.setAttribute("id", "victory");
+newHeader.textContent = "Garrett is the champion";
